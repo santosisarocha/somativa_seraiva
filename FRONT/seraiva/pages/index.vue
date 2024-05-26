6a08cbe3a49a006c7057d7e105c71f790d65de08
santosisarocha <santosisabelarocha@gmail.com>
2024-05-25 23:53:20 -0300

    com chat bot

--- FRONT/seraiva/pages/index.vue
+++ FRONT/seraiva/pages/index.vue
@@ -15,11 +15,25 @@ const atualizarProdutos= () => {
 });
 };
 atualizarProdutos();
+
+const painel = ref();
+
+const toggle = (event: any) => {
+    painel.value.toggle(event);
+}
 </script>
 
 <template>
     <main class="home-container flex flex-column align-items-center justify-content-center">
         <h1>Home</h1>
+
+        <div class="chatbot">
+            <Button type="button" icon="pi pi-comment" label="Chatbot" @click="toggle" />
+            <OverlayPanel ref="painel">
+                <iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/cbf3d547-68ab-4213-9a8c-96cd5336d530"></iframe>
+            </OverlayPanel>
+        </div>
+
         <div class="produtos-container grid align-items-center justify-content-center">
 
             <div v-for="(livro,index) in livros" :key="index">
@@ -40,6 +54,12 @@ atualizarProdutos();
     min-height: calc(100vh - var(--altura-header));
     // min-width: 50vh;
     
+}
+.chatbot{
+    position: fixed;
+    top: 0;
+    right: 0;
+    margin: 1rem
 }
 
 </style>
